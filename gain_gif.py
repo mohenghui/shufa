a65c69be4330612b6f5825c2ecca66bbee64ae08
mohenghui <973733507@qq.com>
2022-04-30 13:50:30 +0800

    优化了一些细节

--- gain_gif.py
+++ gain_gif.py
@@ -13,27 +13,16 @@ import imutils
 """
 
 
-def intermediates(p1, p2, nb_points=8):
-    """"Return a list of nb_points equally spaced points
-    between p1 and p2"""
-    # If we have 8 intermediate points, we have 8+1=9 spaces
-    # between p1 and p2
-    x_spacing = (p2[0] - p1[0]) / (nb_points + 1)
-    y_spacing = (p2[1] - p1[1]) / (nb_points + 1)
-
-    return [[p1[0] + i * x_spacing, p1[1] + i * y_spacing]
-            for i in range(1, nb_points + 1)]
 
 
-def cal_distance(p1, p2):
-    return math.sqrt(math.pow((p2[0] - p1[0]), 2) + math.pow((p2[1] - p1[1]), 2))
+
 
 
 class Gain():
     def __init__(self):
         self.index = 0
         self.save_txt_dir = "./bihua_data/"
-        self.object_name = ["亟", "两", "匧", "辉", "斌", "了"]
+        self.object_name = ["永","亟", "辉", "斌", "了"]
         self.object_type = ".txt"
         self.image_type = ".gif"
         self.save_dir = './pics'
@@ -55,23 +44,40 @@ class Gain():
                 break
         if (self.flag):
             self.f = open(self.save_txt_dir + self.object_name[self.index] + self.object_type, "w")
+            self.flag=False
         else:
             print("全部字体已存在，请进行删除后修改")
             sys.exit()
 
-    def choose_contours(self, contours, MIN_AREA=10):
+    def cal_distance(self,p1, p2):
+        return math.sqrt(math.pow((p2[0] - p1[0]), 2) + math.pow((p2[1] - p1[1]), 2))
+
+    def intermediates(self,p1, p2, nb_points=8):
+        """"Return a list of nb_points equally spaced points
+        between p1 and p2"""
+        # If we have 8 intermediate points, we have 8+1=9 spaces
+        # between p1 and p2
+        x_spacing = (p2[0] - p1[0]) / (nb_points + 1)
+        y_spacing = (p2[1] - p1[1]) / (nb_points + 1)
+
+        return [[p1[0] + i * x_spacing, p1[1] + i * y_spacing]
+                for i in range(1, nb_points + 1)]
+    def choose_contours(self, contours, MIN_AREA=20):
         point_list = []
         contour_list = []
         for item in contours:
             # cv2.boundingRect用一个最小的矩形，把找到的形状包起来
             rect = cv2.boundingRect(item)
-            x = rect[0]
-            y = rect[1]
             width = rect[2]
             height = rect[3]
+            x = (rect[0]*2+width)//2
+            y = (rect[1]*2+height)//2
+            # x=rect[0]
+            # y=rect[1]
             # print(width / height)
             # print(width * height)
             # 计算方块的大小比例,符合的装到集合
+            # print(width*height)
             if (width * height > MIN_AREA):
                 point_list.append((x, y, width, height))
                 # print(width * height)
@@ -88,6 +94,7 @@ class Gain():
         img = Image.open(src_path)
         point_x = -1
         point_y = -1
+        pre_point_x,pre_point_y=-1,-1
         for i in range(img.n_frames):
             img.seek(i)
             # seek() 方法用于移动文件读取指针到指定位置。
@@ -105,8 +112,12 @@ class Gain():
                 currentframe = new_img
                 currentframe_abs = cv2.absdiff(currentframe, previousframe)
                 currentframe_median = cv2.medianBlur(currentframe_abs, 3)
-                kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
-                currentframe_open = cv2.morphologyEx(currentframe_median, cv2.MORPH_OPEN, kernel)
+                kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+                kernel1 = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
+                currentframe_open = cv2.morphologyEx(currentframe_median, cv2.MORPH_OPEN, kernel1)
+                currentframe_close = cv2.morphologyEx(currentframe_open, cv2.MORPH_CLOSE, kernel)
+                currentframe_close = cv2.morphologyEx(currentframe_close, cv2.MORPH_CLOSE, kernel)
+                currentframe_open = cv2.morphologyEx(currentframe_close, cv2.MORPH_OPEN, kernel1)
                 #        img = cv2.imread("E:/chinese_ocr-master/4.png")
                 #        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
                 ret, threshold_frame = cv2.threshold(currentframe_open, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
@@ -119,30 +130,35 @@ class Gain():
                 if (not font_point_list) and (self.flag):
                     if (self.frame_only):
                         # self.txt_add_point(point_x, point_y)
-                        for point_and_point_x, point_and_point_y in intermediates([point_x - 5, point_y - 5],
+                        for point_and_point_x, point_and_point_y in self.intermediates([point_x - 10, point_y + 10],
                                                                                   [point_x, point_y], 12):
                             point_x, point_y = point_and_point_x, point_and_point_y
                             self.txt_add_point(point_x, point_y)
                         # point_x, point_y = point_x+10, point_y+10
                         # self.txt_add_point(point_x, point_y)
                         self.frame_only = False
-
+                    pre_point_x,pre_point_y=point_x,point_y
                     point_x, point_y = -1, -1
                     self.txt_add_point(point_x, point_y)
                     self.flag = False
                 elif (font_point_list):
                     for (x, y, width, height) in font_point_list:
                         if (point_x == -1) and (point_y == -1):
+                            if pre_point_x!=-1 and pre_point_y!=-1 and self.cal_distance([pre_point_x,pre_point_y],[x,y])<=15:
+                                for point_and_point_x, point_and_point_y in self.intermediates([pre_point_x,pre_point_y], [x, y]):
+                                    self.txt_add_point(point_and_point_x, point_and_point_y)
+                                    pre_point_x,pre_point_y=-1,-1
                             point_x, point_y = x, y
                             self.frame_only = True
                         else:
-                            # print("距离",cal_distance([point_x,point_y],[x,y]))
-                            if (cal_distance([point_x, point_y], [x, y]) >= 40):
+                            # print("距离",self.cal_distance([point_x,point_y],[x,y]))
+                            if (self.cal_distance([point_x, point_y], [x, y]) >= 40):
                                 point_x, point_y = x, y
                                 self.txt_add_point(point_x, point_y)
+                            # elif (self.cal_distance([point_x, point_y], [x, y]) <= 5):
                             else:
                                 self.txt_add_point(point_x, point_y)
-                                for point_and_point_x, point_and_point_y in intermediates([point_x, point_y], [x, y]):
+                                for point_and_point_x, point_and_point_y in self.intermediates([point_x, point_y], [x, y]):
                                     point_x, point_y = point_and_point_x, point_and_point_y
                                     self.txt_add_point(point_x, point_y)
                                 point_x, point_y = x, y
@@ -159,6 +175,7 @@ class Gain():
 
     def next_write(self):
         self.f = open(self.save_txt_dir + self.object_name[self.index] + self.object_type, "w")
+        self.flag=False
 
     def collect(self):
         self.gifSplit((self.dir + self.object_name[self.index] + self.image_type), (self.image_type))
@@ -184,7 +201,6 @@ class Gain():
                 if (self.index < len(self.object_name) - 1):
                     self.index += 1
                     self.next_write()
-                    self.flag = True
 
                     print("下一个字[%s]" % self.object_name[self.index])
                 else:
--- recurrent.py
+++ recurrent.py
@@ -1,44 +1,47 @@
-#encoding=utf-8
+# encoding=utf-8
 import cv2
 import numpy as np
 import time
+
+
 class Recurrent:
-    def __init__(self,filename):
+    def __init__(self, filename):
         self.save_txt_dir = "./bihua_data/"
-        self.findname =filename
-        self.object_type=".txt"
-        self.findname_len=len(filename)
-        self.index=0
-        self.width=256
-        self.height=256
-        self.channles=1
-        self.img=np.zeros((self.width,self.height,self.channles),np.uint8)
-        self.f=open(self.save_txt_dir+self.findname[self.index]+self.object_type, "r")
+        self.findname = filename
+        self.object_type = ".txt"
+        self.findname_len = len(filename)
+        self.index = 0
+        self.width = 256
+        self.height = 256
+        self.channles = 1
+        self.img = np.zeros((self.width, self.height, self.channles), np.uint8)
+        self.f = open(self.save_txt_dir + self.findname[self.index] + self.object_type, "r")
+
     def start_write(self):
         line = self.f.readline()  # 调用文件的 readline()方法
         while line:
-            x,y=line.split(",")
-            # print(x,y,end='')
-            if(x!=-1)and(y!=-1):
+            x, y = line.split(",")
+            if (x != "-1") and (y != "-1"):
                 for i in range(self.channles):
                     # self.img[int(x),int(y),i]=0
                     cv2.circle(self.img, (int(x), int(y)), 1, (0), 5, cv2.LINE_AA, 0)
                     # cv2.line(self.img, (int(x), int(y)), 1, (0), 5, cv2.LINE_AA, 0)
                     cv2.imshow("write", self.img)
-                    cv2.waitKey(10)
+                    # cv2.waitKey(1)
             # print(line, end = '')  # 后面跟 ',' 将忽略换行符
             # print(line, end = '')　      # 在 Python 3 中使用
             line = self.f.readline()
         self.f.close()
         # cv2.waitKey()
+
     def main(self):
         self.init_write(self.img)
         # self.img[100, 100, 0] = 0
         print("第一个字[%s]" % self.findname[self.index])
         cv2.namedWindow("write")
         # cv2.setMouseCallback("block", self.wirte)
-        while(1):
-            cv2.imshow("write",self.img)
+        while (1):
+            cv2.imshow("write", self.img)
             k = cv2.waitKey(1) & 0xFF
             if k == ord('q'):
                 break
@@ -51,17 +54,20 @@ class Recurrent:
                 if (self.index < self.findname_len - 1):
                     self.index += 1
                     print("下一个字[%s]" % self.findname[self.index])
-                    self.f = open(self.save_txt_dir+self.findname[self.index] + self.object_type, "r")
+                    self.f = open(self.save_txt_dir + self.findname[self.index] + self.object_type, "r")
                 else:
                     print("没字了")
                     break
 
         cv2.destroyAllWindows()
-    def init_write(self,img_init):
+
+    def init_write(self, img_init):
         for i in range(self.channles):
             img_init[:, :, i] = 255
     # def __enter__(self):
     #
+
+
 if __name__ == '__main__':
-    recurrent=Recurrent(["亟","辉","恒","了","斌","莫"])
+    recurrent = Recurrent(["永", "亟", "辉", "斌","恒", "了", "莫"])
     recurrent.main()
